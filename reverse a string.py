be6073632dc1e6d0f254b866dc413d712b189ada
sudha <sudhachalla04@gmail.com>
2023-10-07 21:09:27 +0530

    modify reverse of a string

--- reverse a string.py
+++ reverse a string.py
@@ -1,5 +1,5 @@
-def reverse_string(input_str):
-    reverse_str=input_str[::-1]
+def reverse_string(intput_str):
+    reverse_str=intput_str[::-1]
     return reverse_str
 input_str=input()
 output_str=reverse_string(input_str)
